068e40a92da64e9dda7719f56a58c1c515bb91b3
eto-enot <66057023+eto-enot@users.noreply.github.com>
2026-02-23 12:48:55 +0300

    list files tool

--- dev-assistant/dev_assistant/tools.py
+++ dev-assistant/dev_assistant/tools.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Annotated, Any, Awaitable, Callable, Dict, Optional
 from llama_index.core.tools import FunctionTool, ToolMetadata
 from llama_index.core.tools.function_tool import AsyncCallable
@@ -106,3 +107,47 @@ class CreateFileTool(FunctionTool):
             raise e
         except Exception as e:
             return f"create_file tool got the error during execution: {str(e)}"
+        
+
+class ListFilesTool(FunctionTool):
+    def __init__(self):
+        tool_metadata = _get_tool_metadata(
+            self._list_files, "list_files",
+            "Use this tool to list files and folders in a given directory. Directories names ends with slash symbol '/'. Cost: 2"
+        )
+        super().__init__(self._list_files, tool_metadata)
+
+    async def _list_files(
+        self,
+        ctx: Context,
+        directory: Annotated[str, 'The directory for which you want to list files and folders'],
+        recursive: Annotated[bool, 'List files for all subdirectories as well'] = False):
+        
+        try:
+            work_dir = await ctx.store.get('work_dir', None)
+            if not work_dir:
+                return f"I cannot list directory '{directory}' since working directory is not set. Please ask user to set working directory."
+            
+            print('work dir is:', work_dir)
+            abs_path = Path(directory).resolve()
+            if not abs_path.exists() or not abs_path.is_dir():
+                return f"Directory {directory} does not exist."
+            
+            files = []
+            if recursive:
+                for item in abs_path.rglob('*'):
+                    rel_path = item.relative_to(abs_path)
+                    name = str(rel_path)
+                    if item.is_dir():
+                        name += '/'
+                    files.append(name)
+            else:
+                for item in abs_path.iterdir():
+                    name = item.name
+                    if item.is_dir():
+                        name += '/'
+                    files.append(name)
+
+            return '\n'.join(files)
+        except Exception as e:
+                return f"list_files tool got the error during execution: {str(e)}"
